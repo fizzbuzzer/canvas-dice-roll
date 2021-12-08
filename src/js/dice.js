98622ec7c15e7f5c0663d6302b6ffd150fffd250
Roman <scribehic@gmail.com>
2021-12-08 23:00:11 +0300

    refactor to class

--- src/js/dice.js
+++ src/js/dice.js
@@ -1,113 +1,155 @@
 (function() {
-  let dice;
-  let rotateX = 0;
-  let rotateY = 0;
-  let rotateXDev = 0;
-  let rotateYDev = 0;
-  let rotateZDev = 0;
-  let face;
-  let ctx;
-  const dots = {
-    center: [50, 50],
-    rightTop: [75, 25],
-    leftTop: [25, 25],
-    leftBottom: [25, 75],
-    rightBottom: [75, 75],
-    centerTop: [50, 25],
-    centerBottom: [50, 75],
-  };
-  const config = [
-    {
-      transform: '',
-      dots: [dots.center],
-    },
-    {
-      transform: 'rotateY(90deg)',
-      dots: [dots.rightTop, dots.leftBottom],
-    },
-    {
-      transform: 'rotateX(90deg)',
-      dots: [dots.center, dots.rightTop, dots.leftBottom],
-    },
-    {
-      transform: 'rotateX(-90deg)',
-      dots: [dots.rightTop, dots.leftBottom, dots.leftTop, dots.rightBottom],
-    },
-    {
-      transform: 'rotateY(-90deg)',
-      dots: [dots.center, dots.rightTop, dots.leftBottom, dots.leftTop, dots.rightBottom],
-    },
-    {
-      transform: 'rotateX(-180deg)',
-      dots: [dots.rightTop, dots.leftBottom, dots.leftTop, dots.rightBottom, dots.centerTop, dots.centerBottom],
-    },
-  ];
-  const shadowDom = `
-  <style>
-    :hover {
-      cursor: pointer;
-    }
-  </style>
-  <slot></slot>
-  `;
-
-  function initDice() {
-    dice = document.querySelector("[data-widget='dice']");
-    dice.style['transformOrigin'] = '50px 50px';
-    dice.style['transformStyle'] = 'preserve-3d';
-    dice.style.width = '100px';
-    dice.style.height = '100px';
-    dice.style.transform = `rotateX(${getRandomAngle()}deg) rotateY(${getRandomAngle()}deg) rotateZ(${getRandomAngle()}deg)`;
-    dice.style.transition = 'transform 4s ease 0s';
-    dice.attachShadow({
-      mode: 'open',
+  class Dice {
+    static #DOTS = Object.freeze({
+      center: [50, 50],
+      rightTop: [75, 25],
+      leftTop: [25, 25],
+      leftBottom: [25, 75],
+      rightBottom: [75, 75],
+      centerTop: [50, 25],
+      centerBottom: [50, 75],
     });
-    dice.shadowRoot.innerHTML = shadowDom;
-    dice.addEventListener('click', onDiceClick);
-    buildDots();
-  }
 
-  function onDiceClick() {
-    rotateX += getRandomAngle(10, 20, 90);
-    rotateY += getRandomAngle(10, 20, 90);
-    rotateXDev = getRandomAngle();
-    rotateYDev = getRandomAngle();
-    rotateZDev = getRandomAngle();
-    dice.style.transform = `rotateX(${rotateX + rotateXDev}deg) rotateY(${rotateY + rotateYDev}deg) rotateZ(${rotateZDev}deg)`;
-  }
+    static #FACES_SET = Object.freeze([
+      {
+        transform: '',
+        dots: [Dice.#DOTS.center],
+      },
+      {
+        transform: 'rotateY(90deg)',
+        dots: [Dice.#DOTS.rightTop, Dice.#DOTS.leftBottom],
+      },
+      {
+        transform: 'rotateX(90deg)',
+        dots: [Dice.#DOTS.center, Dice.#DOTS.rightTop, Dice.#DOTS.leftBottom],
+      },
+      {
+        transform: 'rotateX(-90deg)',
+        dots: [Dice.#DOTS.rightTop, Dice.#DOTS.leftBottom, Dice.#DOTS.leftTop, Dice.#DOTS.rightBottom],
+      },
+      {
+        transform: 'rotateY(-90deg)',
+        dots: [Dice.#DOTS.center, Dice.#DOTS.rightTop, Dice.#DOTS.leftBottom, Dice.#DOTS.leftTop, Dice.#DOTS.rightBottom],
+      },
+      {
+        transform: 'rotateX(-180deg)',
+        dots: [Dice.#DOTS.rightTop, Dice.#DOTS.leftBottom, Dice.#DOTS.leftTop, Dice.#DOTS.rightBottom, Dice.#DOTS.centerTop, Dice.#DOTS.centerBottom],
+      },
+    ]); 
 
-  function getRandomAngle(min = 0, max = 5, multiply = 1) {
-    return multiply * (Math.floor(Math.random() * (max - min + 1)) + min); 
-  }
+    #colors = Dice.#colorService({
+      face: '#fffaf0',
+      border: '#878075',
+      dot: '#000000',
+    });
 
-  function buildDots() {
-    config.forEach((item) => {  
-      face = document.createElement('canvas');
-      face.style.position = 'absolute';
-      face.style['backgroundColor'] = '#ebede5';
-      face.style['borderRadius'] = '4px';
-      face.style.border = '1px solid #878075'
-      face.style.background = '#fffaf0';
-  
-      if(Object.prototype.hasOwnProperty.call(item, 'transform')) {
-        face.style.transform = item.transform;
-      }
-      face.style.transform += ' translateZ(50px)';
-      face.width = 100;
-      face.height = 100;
-      ctx = face.getContext('2d');
-      dice.appendChild(face);
-    
-      item.dots.forEach((dot) => {
-        ctx.beginPath();
-        ctx.arc(...dot, 10, 0, Math.PI * 2); 
-        ctx.fill();
-        ctx.closePath();
+    #node;
+    #rotateX = 0;
+    #rotateY = 0;
+
+    constructor(node) {
+      this.#node = node;
+      this.#colors.face = node.dataset.faceColor;
+      this.#colors.border = node.dataset.borderColor;
+      this.#colors.dot = node.dataset.dotColor;
+    }
+
+    render() {
+      const dice = this.#node;
+      dice.style['transformOrigin'] = '50px 50px';
+      dice.style['transformStyle'] = 'preserve-3d';
+      dice.style.width = '100px';
+      dice.style.height = '100px';
+      dice.style.margin = '20px';
+      dice.style.transform = `rotateX(${Dice.#getRandomAngle()}deg) rotateY(${Dice.#getRandomAngle()}deg) rotateZ(${Dice.#getRandomAngle()}deg)`;
+      dice.style.transition = 'transform 4s ease 0s';
+      dice.attachShadow({
+        mode: 'open',
       });
-    })
+      dice.shadowRoot.innerHTML = `
+        <style>
+          :hover {
+            cursor: pointer;
+          }
+        </style>
+        <slot></slot>
+      `;
+
+      this.#buildDots();
+      dice.addEventListener('click', this.#onDiceClick.bind(this));
+    }
+
+    #buildDots() {
+      Dice.#FACES_SET.forEach((item) => {  
+        const face = document.createElement('canvas');
+        face.style.position = 'absolute';
+        face.style['backgroundColor'] = this.#colors.face;
+        face.style['borderRadius'] = '4px';
+        face.style.border = `1px solid ${this.#colors.border}`
+    
+        if(Object.prototype.hasOwnProperty.call(item, 'transform')) {
+          face.style.transform = item.transform;
+        }
+
+        face.style.transform += ' translateZ(50px)';
+        face.width = 100;
+        face.height = 100;
+        const ctx = face.getContext('2d');
+        this.#node.appendChild(face);
+        ctx.fillStyle = this.#colors.dot;
+      
+        item.dots.forEach((dot) => {
+          ctx.beginPath();
+          ctx.arc(...dot, 10, 0, Math.PI * 2); 
+          ctx.fill();
+          ctx.closePath();
+        });
+      })
+    }
+
+    #onDiceClick() {
+      this.#rotateX += Dice.#getRandomAngle(12, 23, 90);
+      this.#rotateY += Dice.#getRandomAngle(12, 23, 90);
+      let xDeviation = Dice.#getRandomAngle();
+      let yDeviation = Dice.#getRandomAngle();
+      let zDeviation = Dice.#getRandomAngle();
+      this.#node.style.transform = `rotateX(${this.#rotateX + xDeviation}deg) rotateY(${this.#rotateY + yDeviation}deg) rotateZ(${zDeviation}deg)`;
+    }
+
+    static #getRandomAngle(min = 0, max = 5, multiply = 1) {
+      return multiply * (Math.floor(Math.random() * (max - min + 1)) + min); 
+    }
+
+    static #colorService(target) {
+      return new Proxy(target, {
+        set(target, key, value) {
+          if (value !== undefined && isColor(value)) {
+            target[key] = value;
+          }
+          return true;
+        }
+      }); 
+  
+      function isColor(strColor){
+        var style = new Option().style;
+        style.color = strColor;
+        return style.color !== '';
+      }
+    } 
+  }
+
+  function init() {
+    const nodes = document.querySelectorAll("[data-widget='dice']");
+    if (nodes.length === 0) {
+      return;
+    }
+    
+    nodes.forEach(node => {
+      (new Dice(node)).render();
+    });
   }
 
   document.addEventListener('DOMContentLoaded', () => {
-    initDice();
+    init();
   });
 })()
